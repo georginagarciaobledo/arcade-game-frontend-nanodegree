36f05deb2a128a307e049a64fc23a7d8288451e3
georginagarciaobledo <georgina376@gmail.com>
2016-08-31 16:53:37 -0700

    feature: create 4 instancs of enemy, after getting help from ND mentor

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = -101;
+    this.y = 50;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/char-boy.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x = this.x + 100*dt;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -25,12 +27,20 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.x = 450
+    this.y = 250
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var enemy1 = new Player();
+var enemy2 = new Player();
+enemy2.sprite = 'images/char-cat-girl.png';
+var enemy3 = new Player();
+enemy3.sprite = 'images/char-horn-girl.png'
+var enemy4 = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
